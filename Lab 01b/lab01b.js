f4118a643397d5a495f60545e9de16010a790a0e
Mike Felts <mike_felts@yahoo.com>
2022-01-25 11:02:49 -0600

    added console log messages

--- Lab 01b/lab01b.js
+++ Lab 01b/lab01b.js
@@ -2,15 +2,19 @@ console.log("Hello World!");
 
 let userInput1 = prompt("What's your favorite game?");
 alert(`That sounds fun. I've never played ${userInput1}`);
+console.log(userInput1)
 
 let userInput2 = prompt("What do you like to do for fun?");
 alert(`${userInput2} sounds fun.`);
+console.log(userInput2)
 
 let userInput3 = prompt("What is your favorite city?");
 alert(`I'll have to visit ${userInput3} someday.`);
+console.log(userInput3)
 
 let userInput4 = prompt("What is your favorite food?");
 alert(`I like ${userInput4} as well.`);
+console.log(userInput4)
 
 alert(`One day we should play ${userInput1} and ${userInput2}. We need to visit ${userInput3} and eat some ${userInput4}!`);
 
