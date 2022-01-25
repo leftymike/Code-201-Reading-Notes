ed3b045c962f9d4763b31d4a1415788966d64fef
Mike Felts <mike_felts@yahoo.com>
2022-01-24 20:37:09 -0600

    added js prompts

--- Lab 01b/lab01b.js
+++ Lab 01b/lab01b.js
@@ -1,4 +1,19 @@
 console.log("Hello World!");
 
+let userInput1 = prompt("What's your favorite game?");
+alert(`That sounds fun. I've never played ${userInput1}`);
+
+let userInput2 = prompt("What do you like to do for fun?");
+alert(`${userInput2} sounds fun.`);
+
+let userInput3 = prompt("What is your favorite city?");
+alert(`I'll have to visit ${userInput3} someday.`);
+
+let userInput4 = prompt("What is your favorte food?");
+alert(`I like ${userInput4} as well.`);
+
+
+
+
 
 
